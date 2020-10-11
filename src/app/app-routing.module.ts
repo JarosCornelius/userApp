c809b2d084c51b6c05a1941b6a1a57e2b375bd74
Karol <karoljanjaros@gmail.com>
2020-10-11 13:23:19 +0200

    AIO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./Component/home/home.component";
+import {AboutComponent} from "./Component/about/about.component";
+import {ContactComponent} from "./Component/contact/contact.component";
+import {PageNotFoundComponent} from "./Component/page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Component/home/home.component';
+import { AboutComponent } from './Component/about/about.component';
+import { ContactComponent } from './Component/contact/contact.component';
+import {PageNotFoundComponent} from "./Component/page-not-found/page-not-found.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
